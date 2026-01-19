85f310a462aca7b856d025f5ff968e8d79842c12
PLH <1772045545@qq.com>
2026-01-20 00:42:42 +0800

    翻译

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -56,7 +56,7 @@ export function Navbar() {
 
   return (
     <>
-      <DemoBanner />
+      {/*<DemoBanner />*/}
       
       <nav className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
